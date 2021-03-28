56894b4d76d1de7689cd92349821929055856788
Jack Gibson <jackgibson1603@gmail.com>
2021-03-28 14:37:21 +0100

    changing origin name

--- src/View.js
+++ src/View.js
@@ -10,7 +10,7 @@ function View() {
         <MDBView src="images/herobackground.jpg">
         <MDBMask  className="flex-center flex-column text-white text-center">
           <h3 className=""><strong>Welcome to Test Primary School</strong></h3>
-          <h5 className="">A Happy and safe environment for children set in the countryside</h5>
+          <h5 className="">A Happy and safe environment for children set in the countryside.</h5>
         </MDBMask>
       </MDBView>
     )
